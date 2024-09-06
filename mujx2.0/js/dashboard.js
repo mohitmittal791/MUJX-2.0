168fa3b2f33e69c02d537d4cfa4f3cc555739a04
mohitmittal791 <148737410+mohitmittal791@users.noreply.github.com>
2024-09-06 10:21:57 +0530

    Add files via upload

---
+++ mujx2.0/js/dashboard.js
@@ -0,0 +1,4 @@
+document.getElementById('logoutButton').addEventListener('click', function () {
+    // Redirect to another page on logout, e.g., the home page
+    window.location.href = "index.html";
+});
